cd641d571c107d4a9bfa004d27e3bf7a59796f91
Alfredo Miranda <alfredo.miranda.learning@gmail.com>
2025-03-23 14:34:00 -0600

    init project

---
+++ src/main/java/com/arch/library/LibraryCatalogueMsApplication.java
@@ -0,0 +1,13 @@
+package com.arch.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryCatalogueMsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LibraryCatalogueMsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/arch/library/LibraryCatalogueMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arch.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryCatalogueMsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
