19204e9c24970d0fbce01addc86fc2f25231e1a0
Alfredo Miranda <alfredo.miranda.learning@gmail.com>
2025-04-15 16:50:26 -0600

    feat: enable test perfil

--- src/test/java/com/arch/library/LibraryCatalogueMsApplicationTests.java
+++ src/test/java/com/arch/library/LibraryCatalogueMsApplicationTests.java
@@ -2,7 +2,9 @@ package com.arch.library;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class LibraryCatalogueMsApplicationTests {
 
